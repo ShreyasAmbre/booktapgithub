08a015023ca99095aa06c5a380665758053f6788
ShreyasAmbre <shreyasambre00@gmail.com>
2019-12-06 14:53:01 +0530

    Push 2 6th  Dec 19

--- booktap/booktap/urls.py
+++ booktap/booktap/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls import static
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -37,4 +38,4 @@ urlpatterns = [
     path('api/', include('favourite.api_urls')),
 
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
